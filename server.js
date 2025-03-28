06159aba49211b1afca669c11aa0382fe26ed6a6
temp1209 <temp628496@gmail.com>
2025-03-29 03:54:59 +0900

    UUID用にcrypto追加

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const sizeOf = require("image-size");
 const bodyParser = require("body-parser");
 const multer = require("multer");
 const AdmZip = require("adm-zip");
+const crypto = require("crypto");
 
 const app = express();
 const PORT = 3000;
