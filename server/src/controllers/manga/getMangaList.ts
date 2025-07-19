e24071623545dadbadc79da8680ea6435c6cd380
temp1209 <temp628496@gmail.com>
2025-07-19 21:31:15 +0900

    getMangaListをreadJson関数を使ったものに変更

--- server/src/controllers/manga/getMangaList.ts
+++ server/src/controllers/manga/getMangaList.ts
@@ -1,66 +1,50 @@
 import fs from "fs";
-import { Request , Response } from "express";
-import { RawMangaQuerySchema,MangaQuery } from "../../schemas/queriesSchema";
-import { MetadataSchema } from "../../schemas/metadataSchema";
+import { Request, Response } from "express";
+import { RawMangaQuerySchema, MangaQuery, SearchQuery, SearchQuerySchema } from "../../schemas/queriesSchema";
+import { Metadata, MetadataSchema } from "../../schemas/metadataSchema";
 import { decodeQueryParamArray } from "../../utils/query";
 import { SearchableKey } from "../../types/SearchableKey";
 import { paths } from "../../config/paths";
+import { readJsonWithSchema } from "../../utils/readJsonWithSchema";
+import { mapObjectValues } from "../../utils/mapObjectValues";
 
 // Get
 // クエリから検索しマンガ情報を返すAPI
-export const getMangaList = (req: Request, res: Response) => {
+export const getMangaList = async (req: Request, res: Response) => {
   const resultReqParse = RawMangaQuerySchema.safeParse(req.query);
   if (!resultReqParse.success) {
+    console.error(resultReqParse.error.message);
     res.status(400).json({ error: "クエリの形式が不正です" });
     return;
   }
   const query = resultReqParse.data;
 
-  const page = query.pageConf.page
+  const page = query.pageConf.page;
   const limit = query.pageConf.limit;
   const startIndex = (page - 1) * limit;
   const endIndex = page * limit;
 
-  if (isNaN(page) || page < 1) {
-    res.status(400).json({ error: "ページの指定が不正です" });
-    return;
+  const decodedSearchQuery = mapObjectValues(query.search,decodeQueryParamArray);
+
+  let resultData:Metadata = [];
+  try {
+    const metadata = await readJsonWithSchema(paths.data.metadataFile, MetadataSchema, []);
+    resultData = metadata;
+
+    for (const key in decodedSearchQuery) {
+      const typedKey = key as SearchableKey;
+      const queryValues = decodedSearchQuery[typedKey];
+      if (!queryValues) continue;
+
+      resultData = resultData.filter((item) => queryValues.every((v) => item[typedKey].includes(v)));
+    }
+  } catch (e) {
+    console.error("[error:getMangaList]metadataの取得に失敗しました");
+    res.status(500).json({error:"漫画情報の取得に失敗しました"});
   }
 
-  fs.readFile(paths.data.metadataFile, "utf-8", (err, data) => {
-    if (err) {
-      console.error(err);
-      res.status(500).json({ error: "metadataの読み込みに失敗しました" });
-      return;
-    }
+  //ページに切り取り
+  const mangaListPageData = resultData.slice(startIndex, endIndex);
 
-    const resultMetadataParse = MetadataSchema.safeParse(JSON.parse(data));
-    if (!resultMetadataParse.success) {
-      console.error("metadataのパースに失敗しました");
-      res.status(500).json({ error: "metadataのパースに失敗しました" });
-      return;
-    }
-    const allMangaData = resultMetadataParse.data;
-
-    //クエリに合わせて成形
-    let formedData = allMangaData;
-
-    if (query.search) {
-      const decodedSearchQuery = Object.fromEntries(
-        Object.entries(query.search).map(([key, value]) => [key, decodeQueryParamArray(value)])
-      ) as MangaQuery["search"];
-
-      for (const key in decodedSearchQuery) {
-        const typedKey = key as SearchableKey;
-        const queryValues = decodedSearchQuery[typedKey];
-        if (!queryValues) continue;
-
-        formedData = formedData.filter((item) => queryValues.every((v) => item[typedKey].includes(v)));
-      }
-    }
-
-    //ページに切り取り
-    const mangaListPageData = formedData.slice(startIndex, endIndex);
-
-    res.status(200).json({ page, limit, total: formedData.length, data: mangaListPageData });
-  });
+  res.status(200).json({ page, limit, total: resultData.length, data: mangaListPageData });
 };
