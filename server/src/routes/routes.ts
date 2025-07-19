fd8fce9fd7e6155b4c875be2340bcc5947836003
temp1209 <temp628496@gmail.com>
2025-07-19 17:59:51 +0900

    すべてのRoutesフォルダのコードを1ファイルに統合

---
+++ server/src/routes/routes.ts
@@ -0,0 +1,24 @@
+import express from "express";
+const router = express.Router();
+
+
+//manga
+import { getMangaList } from "../controllers/manga/getMangaList";
+import { getPages } from "../controllers/manga/getPages";
+import { getTagList } from "../controllers/manga/getTagList";
+import { postMangaUpload } from "../controllers/manga/postMangaUpload";
+import { multerUpload } from "../middlewares/multerUpload";
+
+router.get("/get-manga-list", getMangaList);
+router.get("/get-pages/:mangaID", getPages);
+router.get("/get-tag-list", getTagList);
+router.post("/post-manga-upload", multerUpload.single("file"), postMangaUpload);
+
+
+
+//config
+import { getServerConfigAPI } from "../controllers/config/getServerConfig";
+
+router.get("/get-user-config", getServerConfigAPI);
+
+export default router;
