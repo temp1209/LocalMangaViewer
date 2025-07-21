35fd9d3dd5806cf4951313c787fd626e6f1b1ed6
temp1209 <temp628496@gmail.com>
2025-07-21 23:53:50 +0900

    全タグリストのカウント関数を分離

--- server/src/controllers/manga/getTagList.ts
+++ server/src/controllers/manga/getTagList.ts
@@ -1,28 +1,10 @@
 import fs from "fs";
-import { Request , Response } from "express";
-import { paths } from "../../config/paths";
-import { MetadataList } from "../../schemas/metadataSchema";
+import { Request, Response } from "express";
+import { countTags } from "../../services/manga/countTags";
 
 // Get
 // タグとその登録数を返すAPI
-export const getTagList = (req:Request, res:Response) => {
-  fs.readFile(paths.data.metadataFile, "utf-8", (err, data) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).json({ error: "Failed to load metadata." });
-    }
-    const allMangaData = JSON.parse(data) as MetadataList;
-    const allTagData = allMangaData.map((item) => item.tags).flat();
-    const tagFrequency = allTagData.reduce<Record<string, number>>((acc, str) => {
-      acc[str] = (acc[str] || 0) + 1;
-      return acc;
-    }, {});
-
-    const sortedTagData = Object.entries(tagFrequency).sort((a, b) => b[1] - a[1]);
-    const tagDataWithCount = sortedTagData.map(([str, count]) => {
-      return { str, count };
-    });
-
-    res.json(tagDataWithCount);
-  });
+export const getTagList = async (req: Request, res: Response) => {
+  const tagList = await countTags();
+  res.status(200).json(tagList);
 };
---
+++ server/src/services/manga/countTags.ts
@@ -0,0 +1,19 @@
+import { paths } from "../../config/paths";
+import { MetadataListSchema } from "../../schemas/metadataSchema";
+import { readJsonWithSchema } from "../../utils/readJsonWithSchema";
+
+export const countTags = async () => {
+  const metadataList = await readJsonWithSchema(paths.data.metadataFile, MetadataListSchema, []);
+  const allTagData = metadataList.map((item) => item.tags).flat();
+  const tagFrequency = allTagData.reduce<Record<string, number>>((acc, str) => {
+    acc[str] = (acc[str] || 0) + 1;
+    return acc;
+  }, {});
+
+  const sortedTagData = Object.entries(tagFrequency).sort((a, b) => b[1] - a[1]);
+  const tagDataWithCount = sortedTagData.map(([str, count]) => {
+    return { str, count };
+  });
+
+  return tagDataWithCount;
+};
