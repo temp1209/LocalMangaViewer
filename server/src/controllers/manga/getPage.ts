a082b5d805a966ef122efc82829fccc6ab2d0bf8
temp1209 <temp628496@gmail.com>
2025-07-30 19:33:04 +0900

    画像を静的配信にしたことで不要になったgetPage系の関数を削除

--- server/src/controllers/manga/getPage.ts
+++
@@ -1,5 +0,0 @@
-import { Request, Response } from "express";
-
-export const getPage = async (req: Request, res: Response) => {
-  
-};
--- server/src/routes/routes.ts
+++ server/src/routes/routes.ts
@@ -5,14 +5,12 @@ const router = express.Router();
 //manga
 import { getMangaListAPI } from "../controllers/manga/getMangaList";
 import { getPageList } from "../controllers/manga/getPageList";
-import { getPage } from "../controllers/manga/getPage";
 import { getTagList } from "../controllers/manga/getTagList";
 import { postMangaUpload } from "../controllers/manga/postMangaUpload";
 import { multerUpload } from "../middlewares/multerUpload";
 
 router.get("/get-manga-list", getMangaListAPI);
 router.get("/get-page/:mangaID", getPageList);
-router.get("/get-page/:mangaID/:page",getPage);
 router.get("/get-tag-list", getTagList);
 router.post("/post-manga-upload", multerUpload.single("file"), postMangaUpload);
 
