544b48275c58335a2f8669ffcd9b729f83de85f9
temp1209 <temp628496@gmail.com>
2025-10-06 16:32:37 +0900

    logger.tsを作成

---
+++ server/src/utils/logger.ts
@@ -0,0 +1,15 @@
+export const log = (...args:any[])=>{
+  console.log("[log]",...args);
+}
+
+export const error = (...args:any[])=>{
+  console.error("[error]",...args);
+}
+
+export const info = (...args:any[])=>{
+  console.info("[info]",...args);
+}
+
+export const warn = (...args:any[])=>{
+  console.warn("[warn]",...args);
+}
