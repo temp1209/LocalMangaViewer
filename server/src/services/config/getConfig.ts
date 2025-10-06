edc1d64f2f879c22fd84ecf7639c188fe6b00740
temp1209 <temp628496@gmail.com>
2025-10-06 12:58:59 +0900

    config.jsonへのパスを修正

--- server/src/services/config/getConfig.ts
+++ server/src/services/config/getConfig.ts
@@ -1,9 +1,10 @@
 import { Config, configSchema, defaultConfig } from "@comic-viewer/shared";
 import { readJsonWithSchemaSafe } from "../../utils/readJsonWithSchema.js";
+import path from "path";
 
-const relativePath = "../../config/config.json";
+const configPath = path.resolve(import.meta.dirname,"../../../config/config.json");
 
 export const getConfig = async (): Promise<Config> => {
-  const data = await readJsonWithSchemaSafe(relativePath, configSchema, defaultConfig);
+  const data = await readJsonWithSchemaSafe(configPath, configSchema, defaultConfig);
   return data;
 };
--- server/src/services/config/setConfig.ts
+++ server/src/services/config/setConfig.ts
@@ -3,7 +3,7 @@ import path from "path";
 import { Config } from "@comic-viewer/shared";
 import { writeJson } from "../../utils/writeJson.js";
 
-const configPath = path.resolve(import.meta.dirname, "../../config/config.json");
+const configPath = path.resolve(import.meta.dirname, "../../../config/config.json");
 
 export const setConfig = async (newConfig: Config) => {
   return await writeJson(configPath, newConfig);
