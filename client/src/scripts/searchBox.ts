16d2e5af73acd30b8ee1fcdce899521792da22b1
temp1209 <temp628496@gmail.com>
2025-12-31 22:56:50 +0900

    searchboxをヘッダーに追加

---
+++ client/src/scripts/searchBox.ts
@@ -0,0 +1,9 @@
+import { SearchQuery } from "@comic-viewer/shared";
+
+const searchBoxButton = document.getElementById("search-box-button");
+const searshInput = document.getElementById("search-input");
+searchBoxButton?.addEventListener("click",()=>{
+  const currentQuery = 
+
+  window.location.href = "";
+})
